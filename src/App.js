314551b1d3b78e0a98e20122cbe9314107f07758
Abdulazeez Abdulazeez Adeshina <youngestdev@naij.com>
2017-12-01 15:32:56 +0100

    git add all

--- src/App.js
+++ src/App.js
@@ -9,22 +9,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Helo Sugar Labs</h1>
+          <h1 className="App-title">Hello Sugar Labs</h1>
         </header>
         <div className=" container paper">
         <h1>About Me</h1>
-        <p>Hello. I'm Abdulazeez. I'm a Nigerian. I'm an upcoming Javascript ninja. I'm a beginner to Open Source. I really love <b>Jollof Rice</b>.. I look on to writng better codes with react as a platform and alos contributing to this great project  Sugar Labs. 
+        <p>Hello. I'm Abdulazeez. I'm a Nigerian. I'm an upcoming Javascript ninja. I'm a beginner in the world of Open Source. I really love <b>Jollof Rice</b>.. This is my first react project so I look on to writing better codes with react as a platform and also contributing to this great project  <b> Sugar Labs. </b>
         </p>
      <h3>My Skills.. </h3>
      <ol>
-
+<p>
      <li>Coding Skills : <progress value="60" max="100"> 60% </progress></li>   
         <li>Writing Skills : <progress value="90" max="100">
 </progress></li>
-         <li>Eating Skills : <progress value="100" max="100">
+        <li>Eating Skills : <progress value="100" max="100">
 </progress></li>
+</p>
         </ol>
-
+<p>Thats all about me basically.. So i plan to improve my coding skills in Javascript generally and then move on to learning javascript frameworks like the likes of React, Angular, NodeJS etc.. Moreover i'm working on being a stack javascript developer..  </p>
         </div>
        
       </div>
